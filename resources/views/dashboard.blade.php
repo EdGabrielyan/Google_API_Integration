d7d0e7f92c92f73f3a5f5bd5902ad6adb63b19a5
Sargis shirikchyan <sargisshirikchyann@gmail.com>
2025-01-20 14:41:20 +0400

    API integration

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <a href=" " style="background-color: #2D738D; color:white; padding: 10px 15px 15px 10px; border-radius: 10px">Connect Google Account</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/google/login', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/google/callback', function () {
+    $user = Socialite::driver('github')->user();
+
+    // $user->token
+});
+
+require __DIR__.'/auth.php';
